bb5607bdd8ae6a80381963dbd5f6996f75f4989f
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-10-26 12:32:12 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean correcto = false;
+        long numIntroducido = 0;
+        while (!correcto) {
+            try {
+                numIntroducido = Integer
+                        .parseInt(System.console().readLine("Por favor, introduzca un número entero: "));
+                if (numIntroducido < 0) {
+                    System.out.println("Introduzca un número positivo.");
+                } else
+                    correcto = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Introduzca un número entero.");
+            } catch (Exception e) {
+                System.out.println("Ha ocurrido un error inesperado.");
+            }
+        }
+        long n = numIntroducido;
+        long n2 = numIntroducido - 1;
+        n = n * n2;
+        while (n2 > 1) {
+            n2--;
+            n *= n2;
+        }
+        if (numIntroducido == 0)
+            n = 1;
+        else if (numIntroducido == 1)
+            n = 1;
+        System.out.println(numIntroducido + "! = " + n);
+    }
+}
